61d3e95bbe9a0f9974a2dddcf21ba0c747f205ca
jam-sut <jis509@york.ac.uk>
2022-12-19 13:54:18 +0000

    Moved some images around and created a prototype large map

--- assets/map/art_map/art_tileset.tsx
+++ assets/map/art_map/art_tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="art_tileset" tilewidth="128" tileheight="128" tilecount="16" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="art_tileset" tilewidth="128" tileheight="64" tilecount="15" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="art_images/cnr.png"/>
@@ -13,32 +13,14 @@
  <tile id="3">
   <image width="64" height="64" source="art_images/ctrtr.png"/>
  </tile>
- <tile id="4">
-  <image width="128" height="128" source="art_images/Double Counter and Salad bar..png"/>
- </tile>
- <tile id="5">
-  <image width="128" height="128" source="art_images/Double Counter w food crate.png"/>
- </tile>
- <tile id="6">
-  <image width="128" height="128" source="art_images/Double Counter.png"/>
- </tile>
  <tile id="7">
   <image width="64" height="64" source="art_images/lettuce.png"/>
  </tile>
  <tile id="8">
   <image width="64" height="64" source="art_images/linectr.png"/>
  </tile>
- <tile id="9">
-  <image width="64" height="128" source="art_images/Oven (clean).png"/>
- </tile>
- <tile id="10">
-  <image width="64" height="128" source="art_images/Oven (messy).png"/>
- </tile>
  <tile id="11">
   <image width="64" height="64" source="art_images/pln.png"/>
- </tile>
- <tile id="12">
-  <image width="64" height="128" source="art_images/Single Counter Sideways.png"/>
  </tile>
  <tile id="13">
   <image width="64" height="64" source="art_images/tomato.png"/>
@@ -48,5 +30,20 @@
  </tile>
  <tile id="15">
   <image width="64" height="64" source="art_images/Top Oven.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="art_images/ovenbotcln.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="art_images/ovenbotmes.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="art_images/oventopmes.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="art_images/ovtopcln.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="64" source="art_images/Salad_top.png"/>
  </tile>
 </tileset>
