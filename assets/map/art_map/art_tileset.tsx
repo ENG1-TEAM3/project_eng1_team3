568d3fe546cb2d59105f3dd23f9d3dfa206541d6
jam-sut <jis509@york.ac.uk>
2023-01-30 18:20:20 +0000

    new trash can

--- assets/map/art_map/art_tileset.tsx
+++ assets/map/art_map/art_tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="art_tileset" tilewidth="128" tileheight="64" tilecount="25" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="art_tileset" tilewidth="128" tileheight="64" tilecount="26" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="64" height="64" source="art_images/cnr.png"/>
@@ -87,5 +87,8 @@
  </tile>
  <tile id="31">
   <image width="128" height="64" source="art_images/chopping.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="art_images/Trash can.png"/>
  </tile>
 </tileset>
