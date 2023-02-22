5ab68783b4330e1ac160a6c577f77f6ea71932ba
Emma <ejh647@york.ac.uk>
2023-02-22 10:05:05 +0000

    Fix gradle

--- tests/src/com/team3gdx/game/tests/AssetTests.java
+++ tests/src/com/team3gdx/game/tests/AssetTests.java
@@ -1,7 +1,7 @@
 package com.team3gdx.game.tests;
 
 import com.badlogic.gdx.Gdx;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -10,11 +10,11 @@ public class AssetTests {
 
     @Test
     public void testBurgerAssetExists() {
-        Assert.assertTrue(Gdx.files.internal("items/burger.png").exists());
+        assertTrue(Gdx.files.internal("items/burger.png").exists());
     }
 
     @Test
     public void testTests() {
-        Assert.assertTrue(true);
+        assertTrue(true);
     }
 }
