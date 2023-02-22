53a4453fd82f4e4e6f3001400044f57f486ceea6
mam661 <mam661@york.ac.uk>
2023-02-22 11:41:20 +0000

    Added a few more tests

--- tests/src/com.team3.gdx.tests/AssetTests.java
+++ tests/src/com.team3.gdx.tests/AssetTests.java
@@ -17,6 +17,36 @@ public class AssetTests {
         assertTrue("Cook 1 hand asset exists test", Gdx.files.internal("entities/cook_walk_hands_1.png").exists());
         assertTrue("Cook 2 hand asset exists test", Gdx.files.internal("entities/cook_walk_hands_1.png").exists());
         assertTrue("Cook 3 hand asset exists test", Gdx.files.internal("entities/cook_walk_hands_1.png").exists());
+    }
+    @Test
+    public void testCustAssetsExist(){
+        //customer 1
+        assertTrue("Customer 1 front", Gdx.files.internal("entities/cust1f.png").exists());
+        assertTrue("Customer 1 back", Gdx.files.internal("entities/cust1b.png").exists());
+        assertTrue("Customer 1 left", Gdx.files.internal("entities/cust1l.png").exists());
+        assertTrue("Customer 2 right", Gdx.files.internal("entities/cust2r.png").exists());
+        //customer 2
+        assertTrue("Customer 2 front", Gdx.files.internal("entities/cust2f.png").exists());
+        assertTrue("Customer 2 back", Gdx.files.internal("entities/cust2b.png").exists());
+        assertTrue("Customer 2 left", Gdx.files.internal("entities/cust2l.png").exists());
+        assertTrue("Customer 2 right", Gdx.files.internal("entities/cust2r.png").exists());
+        //customer 3
+        assertTrue("Customer 3 front", Gdx.files.internal("entities/cust3f.png").exists());
+        assertTrue("Customer 3 back", Gdx.files.internal("entities/cust3b.png").exists());
+        assertTrue("Customer 3 left", Gdx.files.internal("entities/cust3l.png").exists());
+        assertTrue("Customer 3 right", Gdx.files.internal("entities/cust3r.png").exists());
+        //customer 3
+        assertTrue("Customer 4 front", Gdx.files.internal("entities/cust4f.png").exists());
+        assertTrue("Customer 4 back", Gdx.files.internal("entities/cust4b.png").exists());
+        assertTrue("Customer 4 left", Gdx.files.internal("entities/cust4l.png").exists());
+        assertTrue("Customer 4 right", Gdx.files.internal("entities/cust4r.png").exists());
+        //customer 3
+        assertTrue("Customer 5 front", Gdx.files.internal("entities/cust5f.png").exists());
+        assertTrue("Customer 5 back", Gdx.files.internal("entities/cust5b.png").exists());
+        assertTrue("Customer 5 left", Gdx.files.internal("entities/cust5l.png").exists());
+        assertTrue("Customer 5 right", Gdx.files.internal("entities/cust5r.png").exists());
+
+
 
 
     }
