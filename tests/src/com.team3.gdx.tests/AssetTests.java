72088a9f58231d6f2732209835f30f107808ce73
mam661 <mam661@york.ac.uk>
2023-02-22 11:25:54 +0000

    hopefully solved the test stuff.

--- tests/src/com.team3.gdx.tests/AssetTests.java
+++ tests/src/com.team3.gdx.tests/AssetTests.java
@@ -11,6 +11,14 @@ public class AssetTests {
 
     @Test
     public void testChefAssetsExist(){
-        assertTrue("Cook 1 standard asset exists test", Gdx.files.internal("cook_walk_1.png").exists);
+        assertTrue("Cook 1 standard asset exists test", Gdx.files.internal("entities/cook_walk_1.png").exists());
+        assertTrue("Cook 2 standard asset exists test", Gdx.files.internal("entities/cook_walk_2.png").exists());
+        assertTrue("Cook 3 standard asset exists test", Gdx.files.internal("entities/cook_walk_3.png").exists());
+        assertTrue("Cook 1 hand asset exists test", Gdx.files.internal("entities/cook_walk_hands_1.png").exists());
+        assertTrue("Cook 2 hand asset exists test", Gdx.files.internal("entities/cook_walk_hands_1.png").exists());
+        assertTrue("Cook 3 hand asset exists test", Gdx.files.internal("entities/cook_walk_hands_1.png").exists());
+
+
     }
+
 }
