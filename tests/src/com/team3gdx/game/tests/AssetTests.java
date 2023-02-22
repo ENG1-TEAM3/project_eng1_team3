94c83bc686c5805a89856af9a63eb985e2e3ed4e
Emma <ejh647@york.ac.uk>
2023-02-22 10:19:30 +0000

    Entities asset tests

--- tests/src/com/team3gdx/game/tests/AssetTests.java
+++ tests/src/com/team3gdx/game/tests/AssetTests.java
@@ -1,7 +1,7 @@
 package com.team3gdx.game.tests;
 
 import com.badlogic.gdx.Gdx;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -10,11 +10,61 @@ public class AssetTests {
 
     @Test
     public void testBurgerAssetExists() {
-        Assert.assertTrue(Gdx.files.internal("items/burger.png").exists());
+        // Check burger exists
+        assertTrue(Gdx.files.internal("items/burger.png").exists());
     }
 
     @Test
-    public void testTests() {
-        Assert.assertTrue(true);
+    public void testChefAssetExists() {
+        // Check chef walking
+        assertTrue(Gdx.files.internal("entities/cook_walk_1.png").exists());
+        assertTrue(Gdx.files.internal("entities/cook_walk_2.png").exists());
+        assertTrue(Gdx.files.internal("entities/cook_walk_3.png").exists());
+        // Check chef with hands in the air
+        assertTrue(Gdx.files.internal("entities/cook_walk_1.png").exists());
+        assertTrue(Gdx.files.internal("entities/cook_walk_2.png").exists());
+        assertTrue(Gdx.files.internal("entities/cook_walk_3.png").exists());
+    }
+
+    //Check whether customer 1 assets exists
+    @Test
+    public void testCust1AssetExists() {
+        assertTrue(Gdx.files.internal("entities/cust1b.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust1f.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust1l.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust1r.png").exists());
+    }
+    //Check whether customer 2 assets exists
+    @Test
+    public void testCust2AssetExists() {
+        assertTrue(Gdx.files.internal("entities/cust2b.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust2f.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust2l.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust2r.png").exists());
+    }
+    //Check whether customer 3 assets exists
+    @Test
+    public void testCust3AssetExists() {
+        assertTrue(Gdx.files.internal("entities/cust3b.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust3f.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust3l.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust3r.png").exists());
+    }
+    //Check whether customer 4 assets exists
+    @Test
+    public void testCust4AssetExists() {
+        assertTrue(Gdx.files.internal("entities/cust4b.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust4f.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust4l.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust4r.png").exists());
+    }
+
+    //Check whether customer 5 assets exists
+    @Test
+    public void testCust5AssetExists() {
+        assertTrue(Gdx.files.internal("entities/cust5b.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust5f.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust5l.png").exists());
+        assertTrue(Gdx.files.internal("entities/cust5r.png").exists());
     }
 }
