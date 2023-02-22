250ce9258163500ad37d218bd7919d1f17956b14
HERNK <pf693@york.ac.uk>
2023-02-22 11:10:04 +0000

    Revert "Implemented reputation points"
    This reverts commit 469caa21befe9a8ba4777fddd5219c7809ad0e35.

--- core/src/com/team3gdx/game/screen/GameScreen.java
+++ core/src/com/team3gdx/game/screen/GameScreen.java
@@ -48,7 +48,6 @@ public class GameScreen implements Screen {
 	final MainScreen ms;
 
 	public static int currentWave = 0;
-	public static int reputationPoints = 3;
 
 	Rectangle volSlideBackgr;
 	Rectangle volSlide;
@@ -321,7 +320,6 @@ public class GameScreen implements Screen {
 				cc.spawnCustomer();
 			}
 			currentWave++;
-			reputationPoints--;
 			currentWaitingCustomer = null;
 		}
 	}
@@ -352,12 +350,9 @@ public class GameScreen implements Screen {
 		}
 
 		game.batch.begin();
-		game.font.draw(game.batch, "Time in s:", gameResolutionX / 2f, 19 * gameResolutionY / 20f);
 		game.font.draw(game.batch, Long.toString((startTime - timeOnStartup) / 1000),
 				gameResolutionX / 2f + gameResolutionX / 10f, 19 * gameResolutionY / 20f);
-		game.font.draw(game.batch, "Reputation Points:", gameResolutionX / 2f, 18 * gameResolutionY / 20f);
-		game.font.draw(game.batch, Integer.toString((reputationPoints)),
-				gameResolutionX / 2f + gameResolutionX / 10f, 18 * gameResolutionY / 20f);
+		game.font.draw(game.batch, "Time in s:", gameResolutionX / 2f, 19 * gameResolutionY / 20f);
 		game.batch.end();
 	}
 
@@ -636,8 +631,7 @@ public class GameScreen implements Screen {
 	}
 
 	public void checkGameOver() {
-		if (reputationPoints == 0
-				|| currentWave == NUMBER_OF_WAVES + 1) {
+		if (currentWave == NUMBER_OF_WAVES + 1) {
 			game.getLeaderBoardScreen().addLeaderBoardData("PLAYER1",
 					(int) Math.floor((startTime - timeOnStartup) / 1000f));
 			game.resetGameScreen();
@@ -648,7 +642,6 @@ public class GameScreen implements Screen {
 
 	public void resetStatic() {
 		currentWave = 0;
-		reputationPoints = 3;
 	}
 
 	/**
