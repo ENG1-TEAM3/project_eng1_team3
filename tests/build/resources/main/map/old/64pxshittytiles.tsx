448096e2dc6b9fe49682906c12de88c93e86da76
TitaniumTurbine <63096937+TitaniumTurbine@users.noreply.github.com>
2023-02-23 13:02:30 +0000

    tried adding cook movement tests

---
+++ tests/build/resources/main/map/old/64pxshittytiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="64pxshittytiles.tsx" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="64pxshittytiles.png" width="320" height="320"/>
+</tileset>
