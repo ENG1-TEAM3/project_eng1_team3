63a2d7af5c6f66dbd98f1959aa102af8baf4fd65
mam661 <mam661@york.ac.uk>
2023-02-24 13:47:26 +0000

    Added audio and item asset tests

--- tests/src/com.team3.gdx.tests/AssetTests.java
+++ tests/src/com.team3.gdx.tests/AssetTests.java
@@ -51,4 +51,61 @@ public class AssetTests {
 
     }
 
+    @Test
+    public void testAudioAssetsExist() {
+        assertTrue("testmusic in music", Gdx.files.internal("audio/music/testMusic.mp3").exists());
+
+        //music things
+        assertTrue("GameMusic.mp3 in 'uielements?'", Gdx.files.internal("uielements/GameMusic.mp3").exists());
+        assertTrue("GameMusic.ogg in 'uielements?'", Gdx.files.internal("uielements/GameMusic.ogg").exists());
+        assertTrue("MainScreenMusic.ogg in 'uielements?'", Gdx.files.internal("uielements/GameMusic.ogg").exists());
+        assertTrue("music.mp3 in 'uielements?'", Gdx.files.internal("uielements/music.mp3").exists());
+
+        //sound effect things
+        assertTrue("cash-register-opening.mp3 in audio/soundFX", Gdx.files.internal("audio/soundFX/cash-register-opening.mp3").exists());
+        assertTrue("chopping.mp3 in audio/soundFX", Gdx.files.internal("audio/soundFX/chopping.mp3").exists());
+        assertTrue("frying.mp3 in audio/soundFX", Gdx.files.internal("audio/soundFX/frying.mp3").exists());
+        assertTrue("money-collect.mp3 in audio/soundFX", Gdx.files.internal("audio/soundFX/money-collect.mp3").exists());
+        assertTrue("testSoundFX.mp3 in audio/soundFX", Gdx.files.internal("audio/soundFX/testSoundFX.mp3").exists());
+        assertTrue("timer-bell-ring.mp3 in audio/soundFX", Gdx.files.internal("audio/soundFX/timer-bell-ring.mp3").exists());
+
+    }
+
+
+    @Test
+    public void testItemAssets(){
+
+        assertTrue("burger.png in items", Gdx.files.internal("items/burger.png").exists());
+        assertTrue("burger_bun.png in items", Gdx.files.internal("items/burger_bun.png").exists());
+        assertTrue("burger_bun_burned.png in items", Gdx.files.internal("items/burger_bun_burned.png").exists());
+        assertTrue("burger_bun_cooked.png in items", Gdx.files.internal("items/burger_bun_cooked.png").exists());
+        assertTrue("burger_burned.png in items", Gdx.files.internal("items/burger_burned.png").exists());
+        assertTrue("burger_cooked.png in items", Gdx.files.internal("items/burger_cooked.png").exists());
+        assertTrue("lettuce.png in items", Gdx.files.internal("items/lettuce.png").exists());
+        assertTrue("lettuce_chopped.png in items", Gdx.files.internal("items/lettuce_chopped.png").exists());
+        assertTrue("meat.png in items", Gdx.files.internal("items/meat.png").exists());
+        assertTrue("meat overcooked.png in items", Gdx.files.internal("items/meat overcooked.png").exists());
+        assertTrue("meat undercooked.png in items", Gdx.files.internal("items/meat undercooked.png").exists());
+        assertTrue("nil.png in items", Gdx.files.internal("items/nil.png").exists());
+        assertTrue("onion.png in items", Gdx.files.internal("items/onion.png").exists());
+        assertTrue("onion_chopped.png in items", Gdx.files.internal("items/onion_chopped.png").exists());
+        assertTrue("patty.png in items", Gdx.files.internal("items/patty.png").exists());
+        assertTrue("patty_burned.png in items", Gdx.files.internal("items/patty_burned.png").exists());
+        assertTrue("patty_cooked.png in items", Gdx.files.internal("items/patty_cooked.png").exists());
+        assertTrue("salad.png in items", Gdx.files.internal("items/salad.png").exists());
+        assertTrue("tomato.png in items", Gdx.files.internal("items/tomato.png").exists());
+        assertTrue("tomato_burned.png in items", Gdx.files.internal("items/tomato_burned.png").exists());
+        assertTrue("tomato_chopped.png in items", Gdx.files.internal("items/tomato_chopped.png").exists());
+        assertTrue("tomato_cooked.png in items", Gdx.files.internal("items/tomato_cooked.png").exists());
+        assertTrue("unformed_patty.png in items", Gdx.files.internal("items/unformed_patty.png").exists());
+
+
+
+
+
+
+
+    }
+
+
 }
