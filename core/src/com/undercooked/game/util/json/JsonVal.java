7c40ca46069f7c7476d6b2289e46967e3c0ae761
hs1955 <hs1955@york.ac.uk>
2023-03-17 11:38:07 +0000

    Middway through making leaderbaord.java. I need to read up on how the LibJDX json library works.

--- core/src/com/undercooked/game/util/json/JsonVal.java
+++ core/src/com/undercooked/game/util/json/JsonVal.java
@@ -1,17 +1,18 @@
 package com.undercooked.game.util.json;
 
-import com.badlogic.gdx.utils.JsonValue;
-
 public abstract class JsonVal<T> {
     protected String ID;
     protected T value;
+
     public JsonVal(String ID, T value) {
         this.ID = ID;
         this.value = value;
     }
+
     public String getID() {
         return ID;
     }
+
     public T getValue() {
         return value;
     }
