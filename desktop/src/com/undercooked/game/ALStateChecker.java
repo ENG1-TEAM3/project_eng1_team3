321cd0a7962aaff6fbab7a28085c05b902248166
OwenTho <owen_thomas@btinternet.com>
2023-03-28 16:31:37 +0100

    Added JavaDocs to the desktop launcher classes.

--- desktop/src/com/undercooked/game/ALStateChecker.java
+++ desktop/src/com/undercooked/game/ALStateChecker.java
@@ -3,7 +3,17 @@ package com.undercooked.game;
 import com.undercooked.game.audio.SoundStateChecker;
 import org.lwjgl.openal.AL10;
 
+/**
+ * A class used to check the ALState within the {@link AL10} class,
+ * that can only be accessed in the desktop launcher folder.
+ */
 public class ALStateChecker implements SoundStateChecker {
+    /**
+     * Returns whether a sound id is playing by its id, or not.
+     * @param soundID {@code int} : The id of the {@link com.badlogic.gdx.audio.Sound}.
+     * @return {@code boolean} : {@code true} if the {@link com.badlogic.gdx.audio.Sound} is playing,
+     *                           {@code false} if it is not.
+     */
     @Override
     public boolean isPlaying(int soundID) {
         // System.out.println(AL10.alGetSourcei(soundID, AL10.AL_SOURCE_STATE));
--- desktop/src/com/undercooked/game/DesktopLauncher.java
+++ desktop/src/com/undercooked/game/DesktopLauncher.java
@@ -4,7 +4,15 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+
+/**
+ * The Launcher class for the game.
+ */
 public class DesktopLauncher {
+	/**
+	 * The main method for the entire program.
+	 * @param arg {@link String} : The arguments for the program.
+	 */
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
