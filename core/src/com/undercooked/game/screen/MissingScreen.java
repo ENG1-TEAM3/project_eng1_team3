3d6e91f3a51f872c654d651564bd978999ba580e
OwenTho <owen_thomas@btinternet.com>
2023-04-19 10:03:55 +0100

    Added a timer to the missing screen to return to the main screen after a second if the game gets stuck there.

--- core/src/com/undercooked/game/screen/MissingScreen.java
+++ core/src/com/undercooked/game/screen/MissingScreen.java
@@ -1,8 +1,11 @@
 package com.undercooked.game.screen;
 
 import com.undercooked.game.MainGameClass;
+import com.undercooked.game.util.Constants;
+import jdk.vm.ci.meta.Constant;
 
 public class MissingScreen extends Screen {
+    float timeCount;
 
     protected MissingScreen(MainGameClass game) {
         super(game);
@@ -20,12 +23,16 @@ public class MissingScreen extends Screen {
 
     @Override
     public void show() {
-
+        timeCount = 0;
     }
 
     @Override
     public void render(float delta) {
-
+        // If this screen is ever opened, go to the main screen after 1 second
+        timeCount += delta;
+        if (timeCount >= 1) {
+            getScreenController().setScreen(Constants.MAIN_SCREEN_ID);
+        }
     }
 
     @Override
