4449e5cd2d622b7b2633f8a7ce8bfd3886a8e4ac
hs1955 <hs1955@york.ac.uk>
2023-04-19 15:03:46 +0100

    Added method saveGame into SaveGame.java

--- core/src/com/undercooked/game/SaveGame.java
+++ core/src/com/undercooked/game/SaveGame.java
@@ -3,6 +3,7 @@ package com.undercooked.game;
 import com.badlogic.gdx.utils.JsonValue;
 import com.undercooked.game.entity.cook.CookController;
 import com.undercooked.game.entity.customer.CustomerController;
+import com.undercooked.game.files.FileControl;
 import com.undercooked.game.station.StationController;
 
 public class SaveGame {
@@ -13,5 +14,11 @@ public class SaveGame {
 		save.addChild("stations", stationController.serializeStations());
 		save.addChild("customers", customerController.serializeCustomers());
 		return save;
+	}
+
+	public static void saveGame(CookController cookController, StationController stationController,
+			CustomerController customerController) {
+		JsonValue save = serializeGame(cookController, stationController, customerController);
+		FileControl.saveJsonData("save.json", save);
 	}
 }
