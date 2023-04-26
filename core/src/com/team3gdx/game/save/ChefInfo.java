454a201afe256b810b215d1373ac18a36eb47e64
Toby <toby@tobymeehan.com>
2023-04-26 14:58:39 +0100

    Add initial save service
    Added service to save games to prefs with json

---
+++ core/src/com/team3gdx/game/save/ChefInfo.java
@@ -0,0 +1,6 @@
+package com.team3gdx.game.save;
+
+public class ChefInfo {
+    public int x;
+    public int y;
+}
---
+++ core/src/com/team3gdx/game/save/GameInfo.java
@@ -0,0 +1,10 @@
+package com.team3gdx.game.save;
+
+import java.util.List;
+
+public class GameInfo {
+    public List<ChefInfo> chefs;
+    public float money;
+    public float timer;
+    public String id;
+}
