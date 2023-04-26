2127b7d4d3bf91f4096cada9d78a005c808f1364
hs1955 <hs1955@york.ac.uk>
2023-04-26 11:41:52 +0100

    Removed SoundStateChecker from MainGameClass Constructor

---
+++ tests/src/de/tomgrill/gdxtesting/tests/MainGameClassTest.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright 2015 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.tomgrill.gdxtesting.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.badlogic.gdx.Gdx;
+import com.undercooked.game.MainGameClass;
+import com.undercooked.game.audio.SoundStateChecker;
+
+import de.tomgrill.gdxtesting.GdxTestRunner;
+
+@RunWith(GdxTestRunner.class)
+public class MainGameClassTest {
+	@Test
+	public void checkConstructorLoadsEverything() {
+		MainGameClass game = new MainGameClass();
+		assertEquals(1, 1);
+	}
+
+}
