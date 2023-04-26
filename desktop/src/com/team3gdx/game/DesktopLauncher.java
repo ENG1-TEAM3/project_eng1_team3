34617ce40a0e305a8493508df3f2241e0bfaf480
Emma <ejh647@york.ac.uk>
2023-04-26 11:57:50 +0100

    Change fps

--- desktop/src/com/team3gdx/game/DesktopLauncher.java
+++ desktop/src/com/team3gdx/game/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(30);
+		config.setForegroundFPS(60);
 		config.setTitle("eng1team3game");
 		// config.setWindowedMode(1920,1080);
 		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
