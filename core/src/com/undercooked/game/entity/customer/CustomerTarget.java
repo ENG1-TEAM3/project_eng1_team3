730685b5e0dbd131975ddb71608b8c801b97f3c6
OwenTho <owen_thomas@btinternet.com>
2023-04-28 17:48:30 +0100

    JavaDocs for CustomerTarget complete.

--- core/src/com/undercooked/game/entity/customer/CustomerTarget.java
+++ core/src/com/undercooked/game/entity/customer/CustomerTarget.java
@@ -1,7 +1,20 @@
 package com.undercooked.game.entity.customer;
 
+/**
+ * An enum for spawning {@link Customer}s changing which
+ * {@link com.undercooked.game.map.Register} that they target.
+ */
 public enum CustomerTarget {
+    /**
+     * Target the farthest {@link com.undercooked.game.map.Register} from the {@link Customer}.
+     */
     FARTHEST,
+    /**
+     * Target the closest {@link com.undercooked.game.map.Register} to the {@link Customer}.
+     */
     CLOSEST,
+    /**
+     * Target a random {@link com.undercooked.game.map.Register}.
+     */
     RANDOM
 }
