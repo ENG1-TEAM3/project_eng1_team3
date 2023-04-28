add75853eaad46230696b302ade0d629a734418b
OwenTho <owen_thomas@btinternet.com>
2023-04-28 21:18:29 +0100

    JavaDocs for GameType complete.

--- core/src/com/undercooked/game/GameType.java
+++ core/src/com/undercooked/game/GameType.java
@@ -1,6 +1,20 @@
 package com.undercooked.game;
 
+/**
+ * The different game modes that
+ * can be played.
+ */
 public enum GameType {
+    /**
+     * Scenario mode, where a set number of
+     * {@link com.undercooked.game.entity.customer.Customer}s have
+     * to be served before time runs out.
+     */
     SCENARIO,
+    /**
+     * Endless mode, where the player has to serve as many
+     * {@link com.undercooked.game.entity.customer.Customer}s before
+     * they run out of reputation.
+     */
     ENDLESS
 }
