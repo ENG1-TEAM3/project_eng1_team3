3d29d099426dea456437768cac00f048fe0d544c
OwenTho <owen_thomas@btinternet.com>
2023-04-28 15:36:57 +0100

    JavaDocs for Customer complete.

--- core/src/com/undercooked/game/entity/customer/Customer.java
+++ core/src/com/undercooked/game/entity/customer/Customer.java
@@ -13,12 +13,26 @@ import com.undercooked.game.map.MapManager;
 import com.undercooked.game.map.Register;
 import com.undercooked.game.util.Listener;
 
+/**
+ * The class used for the {@link Customer}s that appear on the map,
+ * make a {@link Request} and then must be served before their
+ * wait timer runs out.
+ */
 public class Customer {
+
+	/** The x position of the {@link Customer}. */
 	public float x;
+
+	/** The x position of the {@link Customer}. */
 	public float y;
+
+	/** The front facing {@link Texture} of the {@link Customer}. */
 	Texture textf;
+	/** The back facing {@link Texture} of the {@link Customer}. */
 	Texture textb;
+	/** The right facing {@link Texture} of the {@link Customer}. */
 	Texture textr;
+	/** The left facing {@link Texture} of the {@link Customer}. */
 	Texture textl;
 
 	/** The current {@link Texture} that the {@link Customer} should draw. */
@@ -26,27 +40,49 @@ public class Customer {
 
 	/** How far the {@link Customer} moves per second. */
 	float moveSpeed;
-	public boolean leaving;
 
+	/** The opacity that the {@link Customer} is drawn at. */
 	float visibility;
+
+	/** The {@link Register} that the {@link Customer} is linked to. */
 	private Register register;
+
+	/** The {@link Listener} to call when the {@link Customer} was served successfully. */
 	Listener<Customer> servedListener;
+
+	/** The {@link Listener} to call when the {@link Customer} wasn't served successfully. */
 	Listener<Customer> failedListener;
+
+	/** The {@link CustomerController} that is controlling the {@link Customer}. */
 	CustomerController customerController;
 
+	/** The {@link Customer}'s {@link Request}. */
 	public Request order;
+
+	/** The time that the {@link Customer} has left before they leave. */
 	public float waitTimer;
+
+	/** The speed that the {@link #waitTimer} decreases per second. */
 	public float waitSpeed;
+
+	/** Whether the {@link Customer} is waiting ({@code true}) or not ({@code false})*/
 	public boolean waiting;
 
+	/** Whether the {@link Customer} is leaving ({@code true}) or not ({@code false}). */
+	public boolean leaving;
+
+	/** The {@link Customer}'s {@link Texture} number. */
 	private int custNo;
 
 	/**
 	 * Constructor for customer class
 	 * 
-	 * @param custno         - customer number - changes texture
-	 * @param textureManager The {@link TextureManager} to use
-	 *                       to load and get {@link Texture}s from.
+	 * @param custno 			 {@code int} : The {@link Customer}'s {@link Texture} id.
+	 * @param customerController {@link CustomerController} : The controller for the
+	 *                                            {@link Customer}s, so it can be told
+	 *                                            when this {@link Customer} has left.
+	 * @param textureManager {@link TextureManager} : The {@link TextureManager} to use
+	 *                       					  to load and get {@link Texture}s from.
 	 */
 	public Customer(int custno, CustomerController customerController, TextureManager textureManager) {
 		TextureManager assetManager = textureManager;
@@ -77,6 +113,12 @@ public class Customer {
 		this.waitSpeed = 1F;
 	}
 
+	/**
+	 * Update function of the {@link Customer}.
+	 * <br>Updates their position if they're entering or leaving, or
+	 * lowers their wait timer if they have one.
+	 * @param delta {@code float} : The time since the last frame.
+	 */
 	public void update(float delta) {
 		// If leaving, and y <= 0, then make invisible before deleting
 		if (leaving) {
@@ -134,18 +176,35 @@ public class Customer {
 		}
 	}
 
-	public void load(TextureManager textureManager, String textureID) {
+	/**
+	 * Load the {@link Request} of the {@link Customer} if
+	 * they have one.
+	 * @param textureManager {@link TextureManager} : The {@link TextureManager} to
+	 *                                                use
+	 * @param textureGroup {@link String} : The texture group to load to.
+	 */
+	public void load(TextureManager textureManager, String textureGroup) {
 		if (getRequest() != null) {
-			getRequest().load(textureManager, textureID);
+			getRequest().load(textureManager, textureGroup);
 		}
 	}
 
+	/**
+	 * Post load which loads the {@link Request} of the {@link Customer} if
+	 * they have one.
+	 * @param textureManager {@link TextureManager} : The {@link TextureManager} to
+	 *                                                use
+	 */
 	public void postLoad(TextureManager textureManager) {
 		if (getRequest() != null) {
 			getRequest().postLoad(textureManager);
 		}
 	}
 
+	/**
+	 * Draw the {@link Customer}'s {@link Texture}.
+	 * @param batch {@link SpriteBatch} : The {@link SpriteBatch} to use.
+	 */
 	public void draw(SpriteBatch batch) {
 		batch.setColor(1, 1, 1, visibility);
 		// Draw a different texture depending on which direction the Customer is facing
@@ -153,6 +212,10 @@ public class Customer {
 		batch.setColor(1, 1, 1, 1);
 	}
 
+	/**
+	 * Draw the {@link Customer}'s shapes.
+	 * @param shape {@link ShapeRenderer} : The {@link ShapeRenderer} to use.
+	 */
 	public void draw(ShapeRenderer shape) {
 		// Only continue if waiting, as this is used to
 		// draw the wait timer
@@ -173,6 +236,16 @@ public class Customer {
 		shape.setColor(Color.WHITE);
 	}
 
+	/**
+	 * Called when the {@link Customer} is served an item.
+	 * Returns {@code true} or {@code false} whether the {@link Customer}
+	 * was served the correct item or not.
+	 * <br>If served the correct item, it will leave.
+	 * @param item {@link Item} : The {@link Item} that the {@link Customer}
+	 *                            was served.
+	 * @return {@code boolean} : {@code true} if the {@link Item} was correct,
+	 * 							 {@code false} if not.
+	 */
 	public boolean serve(Item item) {
 		if (item.getID().equals(this.order.itemID)) {
 			// Remove this customer from the register
@@ -186,7 +259,12 @@ public class Customer {
 		return false;
 	}
 
+	/**
+	 * Called when the {@link Customer} should leave the map.
+	 */
 	protected void leave() {
+		// If it's already leaving, return
+		if (leaving) return;
 		// Set the Customer to leave
 		leaving = true;
 		// And that they are no longer waiting
@@ -200,15 +278,31 @@ public class Customer {
 		curTexture = textb;
 	}
 
+	/**
+	 * Set the {@link Customer}'s {@link Request}.
+	 * @param request {@link Request} : The {@link Request} to set to.
+	 */
 	public void setRequest(Request request) {
 		this.order = request;
 		this.waitTimer = request.getTime();
 	}
 
+	/**
+	 * Set the {@link Register} that the {@link Customer} is
+	 * linked to.
+	 * @param register {@link Register} : The {@link Register} that
+	 *                                 the {@link Customer} should be linked
+	 *                                 to.
+	 */
 	public void setRegister(Register register) {
 		this.register = register;
 	}
 
+	/**
+	 * Set the move speed of the {@link Customer} as how many
+	 * grid spaces on the {@link com.undercooked.game.map.Map} it moves per second.
+	 * @param moveSpeed {@code float} : The speed to move at.
+	 */
 	public void setMoveSpeed(float moveSpeed) {
 		// Convert it to cells / second
 		moveSpeed = MapManager.gridToPos(moveSpeed);
@@ -216,26 +310,62 @@ public class Customer {
 		this.moveSpeed = Math.max(1F, moveSpeed);
 	}
 
+	/**
+	 * Returns the X position of the {@link Customer}.
+	 * @return {@code float} : The x position.
+	 */
 	public float getX() {
 		return x;
 	}
 
+	/**
+	 * Returns the Y position of the {@link Customer}.
+	 * @return {@code float} : The y position.
+	 */
 	public float getY() {
 		return y;
 	}
 
+	/**
+	 * Returns the {@link Request} of the {@link Customer}.
+	 * @return {@link Register} : The {@link Customer}'s {@link Request}.
+	 */
 	public Request getRequest() {
 		return order;
 	}
 
+	/**
+	 * Returns the {@link Register} that the {@link Customer}
+	 * is linked to.
+	 * @return {@link Register} : The {@link Register}.
+	 */
 	public Register getRegister() {
 		return register;
 	}
 
+	/**
+	 * Returns if the {@link Customer} is waiting or not.
+ 	 * @return {@code boolean} : {@code true} if the {@link Customer} is waiting,
+	 * 							 {@code false} if not.
+	 */
 	public boolean isWaiting() {
 		return waiting;
 	}
 
+	/**
+	 * Set the speed at which a customer will wait. It is the amount
+	 * that the {@link #waitTimer} is lowered per second.
+	 * <br>Default is 1.
+	 * @param waitSpeed {@code float} : The speed to wait at.
+	 */
+	public void setWaitSpeed(float waitSpeed) {
+		this.waitSpeed = Math.max(0, waitSpeed);
+	}
+
+	/**
+	 * Serialise the {@link Customer} into a {@link JsonValue}.
+	 * @return {@link JsonValue} : The {@link Customer}'s data in Json form.
+	 */
 	public JsonValue serial() {
 		if (leaving) return null;
 		// Return JsonValue
@@ -251,9 +381,5 @@ public class Customer {
 		customerRoot.addChild("move_speed", new JsonValue(moveSpeed));
 		customerRoot.addChild("request", order.serial());
 		return customerRoot;
-	}
-
-	public void setWaitSpeed(float waitSpeed) {
-		this.waitSpeed = Math.max(0, waitSpeed);
 	}
 }
