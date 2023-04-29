2b1d889864a61ae41e1f17a9e3c54c230acdb7a8
TobyMeehan <toby@tobymeehan.com>
2023-04-29 15:38:56 +0100

    Add save service exception handling

--- core/src/com/team3gdx/game/save/SaveService.java
+++ core/src/com/team3gdx/game/save/SaveService.java
@@ -2,6 +2,7 @@ package com.team3gdx.game.save;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.utils.Array;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -15,20 +16,20 @@ public class SaveService {
         mapper = new ObjectMapper();
     }
 
-    public List<GameInfo> getSavedGames() {
+    public Array<GameInfo> getSavedGames() {
 
         Preferences prefs = Gdx.app.getPreferences("saves");
 
         Map<String, ?> games = prefs.get();
 
-        ArrayList<GameInfo> result = new ArrayList<>();
+        Array<GameInfo> result = new Array<>();
 
         for (String id : games.keySet()) {
             if (games.get(id) instanceof String) {
                 try {
                     result.add(mapper.readValue((String)games.get(id), GameInfo.class));
-                } catch (JsonProcessingException ignored) {
-
+                } catch (JsonProcessingException ex) {
+                    System.out.println(ex.getMessage());
                 }
             }
         }
@@ -36,13 +37,20 @@ public class SaveService {
         return result;
     }
 
-    public void saveGame(GameInfo game) {
+    public void saveGame(GameInfo game) throws Exception {
         Preferences prefs = Gdx.app.getPreferences("saves");
 
         String id = UUID.randomUUID().toString();
 
-        String json = mapper.writeValueAsString(game);
+        String json = null;
+        try {
+            json = mapper.writeValueAsString(game);
+        } catch (JsonProcessingException e) {
+            throw new Exception("Game data serialisation failed.");
+        }
 
         prefs.putString(id, json);
+
+        prefs.flush();
     }
 }
